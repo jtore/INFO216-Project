96305b453690d486c362dc4ed3f8012d23551322
peterkolbeinsen <79566544+peterkolbeinsen@users.noreply.github.com>
2021-02-24 10:04:49 +0100

    første fil

--- queryFile.py
+++ queryFile.py
@@ -5,10 +5,25 @@ import owlrl
 g = Graph()
 g=g.parse(location="dataset_36.txt", format="turtle")
 
-ex = Namespace("http://example.org/")
-g.bind("ex",ex)
+nh = Namespace("https://newshunter.uib.no/resource#")
+##nhterm = Namespace("https://newshunter.uib.no/term#")
 
-rdfs = owlrl.RDFSClosure.RDFS_Semantics(g,False, False, False)
+
+g.bind("nh", nh)
+##g.bind("nhterm", nhterm)
+
+rdfs = owlrl.RDFSClosure.RDFS_Semantics(g, False, False, False)
 rdfs.closure()
 rdfs.flush_stored_triples()
+
+ann = g.query("""
+    PREFIX nhterm: <https://newshunter.uib.no/term#>
+    ASK{
+        nh:11febd8d-7d7e-4efd-8d47-0353286790e6 nhterm:Item nhterm:hasAnnotation; 
+    }
+""")
+
+print(bool(ann))
+
+
 
