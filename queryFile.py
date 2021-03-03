256721a34558cf81d78f6e453dba43aeba92f821
Joakim Torsvik <jotto-96@hotmail.com>
2021-03-03 09:27:19 +0100

    datetime query

--- queryFile.py
+++ queryFile.py
@@ -6,24 +6,46 @@ g = Graph()
 g=g.parse(location="dataset_36.txt", format="turtle")
 
 nh = Namespace("https://newshunter.uib.no/resource#")
-##nhterm = Namespace("https://newshunter.uib.no/term#")
+nhterm = Namespace("https://newshunter.uib.no/term#")
 
 
 g.bind("nh", nh)
-##g.bind("nhterm", nhterm)
+g.bind("nhterm", nhterm)
+
+
+DateTime = g.query("""
+    PREFIX nhterm: <https://newshunter.uib.no/term#>
+    SELECT ?b ?c
+    WHERE
+    {
+       nh:040f153c-136e-4dbe-99c7-7cde19e27c6b ?b ?c;
+       nhterm:sourceDateTime ?c
+       
+    }
+""")
+
+ann = g.query("""
+    PREFIX nhterm: <https://newshunter.uib.no/term#>
+    SELECT ?b ?c
+    WHERE
+    {
+       nh:040f153c-136e-4dbe-99c7-7cde19e27c6b ?b ?c;
+       nhterm:sourceDateTime ?c
+
+    }
+""")
+
+
+
+for row in ann:
+    print("%s knows %s" % row)
+
 
 rdfs = owlrl.RDFSClosure.RDFS_Semantics(g, False, False, False)
 rdfs.closure()
 rdfs.flush_stored_triples()
 
-ann = g.query("""
-    PREFIX nhterm: <https://newshunter.uib.no/term#>
-    ASK{
-        nh:11febd8d-7d7e-4efd-8d47-0353286790e6 nhterm:Item nhterm:hasAnnotation; 
-    }
-""")
 
-print(bool(ann))
 
 
 
