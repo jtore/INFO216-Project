675b749eec3291e946d4728351c380dd54acd752
Skullkid <jotto-96@hotmail.com>
2021-04-14 17:36:17 +0200

    Query for item

--- queryFile.py
+++ queryFile.py
@@ -1,10 +1,12 @@
 from rdflib import Graph, Namespace, BNode, URIRef, Literal
 from rdflib.namespace import RDF, RDFS, XSD, FOAF, OWL
-
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 import owlrl
 
+"""
 g = Graph()
+
 
 # Bind prefix and namespace
 nh = Namespace("https://newshunter.uib.no/resource")
@@ -20,16 +22,17 @@ dbpedia = URIRef("http://dbpedia.org/resource/")
 newshunter_resource = ("https://newshunter.uib.no/resource/unresolved#")
 
 
+
 def build_graph():
 
-    print("Program start, please select search condition:")
+    #print("Program start, please select search condition:")
 
-    anchorof_value = input("Input anchorOf value for your graph: ")
-    hasentity_value = input("Input entity value of graph: ")
+    anchorof_value = "Tesla" #input("Input anchorOf value for your graph: ")
+    hasentity_value = "Nikola_Tesla" #input("Input entity value of graph: ")
 
     # Item
-    g.add((nh.ID, RDF.type, nhterm.Item))
-    g.add((nh.ID, nhterm.sourceDateTime, Literal("0001-01-01T00:00:00+00:00", datatype=XSD.dateTime)))
+    #g.add((nh.ID, RDF.type, nhterm.Item))
+    #g.add((nh.ID, nhterm.sourceDateTime, Literal("0001-01-01T00:00:00+00:00", datatype=XSD.dateTime)))
 
     # Annotation
     g.add((nhterm.hasAnnotation, RDF.type, bn))
@@ -51,11 +54,11 @@ def build_graph():
 
     print(g.serialize(format="ttl").decode("utf-8"))
 
-
 build_graph()
+"""
 
 
-from SPARQLWrapper import SPARQLWrapper, JSON
+# Queries!
 
 # This creates a server connection to the same URL that contains the graphic interface for Blazegraph.
 # You also need to add "sparql" to end of the URL like below.
@@ -83,6 +86,7 @@ DateTime = sparql.query().convert()
     #print(result["b"]["value"], "knows", result["c"]["value"])
 
 
+# Query for alle tripler
 sparql.setQuery("""
     PREFIX nhterm: <https://newshunter.uib.no/term#>
     
@@ -115,5 +119,48 @@ sourceIRL = sparql.query().convert()
  #   print(result["b"]["value"], result["c"]["value"])
 
 
+#for result in value["results"]["bindings"]:
+ #   print(result)
+
+# Query for å finne et nhterm:Item basert på en verdi i anchorOf inne i det itemet.
+
+anchorof_value = "Tesla"
+
+sparql.setQuery(
+    """
+    PREFIX nhterm: <https://newshunter.uib.no/term#>
+    SELECT ?item ?annotation WHERE {
+        ?item nhterm:hasAnnotation ?annotation .
+        ?annotation nhterm:anchorOf ?anchorvalue .
+        
+        FILTER(STR(?anchorvalue) = "%s")
+    }
+    """
+%anchorof_value)
+
+sparql.setReturnFormat(JSON)
+test = sparql.query().convert()
+for result in test["results"]["bindings"]:
+    print(result["item"]["value"])
 
 
+# Funker ikke, men er en begynnelse
+
+'''
+for result in test["results"]["bindings"]:
+    res = result["item"]["value"]
+    sparql.setQuery(
+        """
+        PREFIX nhterm: <https://newshunter.uib.no/term#>
+        SELECT nhterm:hasAnnotation WHERE {
+            ?item nhterm:hasAnnotation ?annotation .
+            FILTER(STR(?item) = "%s")
+        }
+        """
+        %res)
+    sparql.setReturnFormat(JSON)
+    test2 = sparql.query().convert()
+
+    for result2 in test2["results"]["bindings"]:
+        print(result2["results"]["item"])
+'''
