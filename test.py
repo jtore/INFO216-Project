7ad738cc1b0b262dd0a6f753593510318fd0b90e
Skullkid <jotto-96@hotmail.com>
2021-05-04 12:41:57 +0200

    Test

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import string
 from SPARQLWrapper import SPARQLWrapper, JSON, RDFXML, XML
 import re
 
+
 # java -server -Xmx4g -jar blazegraph.jar
 
 
@@ -22,21 +23,17 @@ def generate_hash():
 
 event_hash_value = generate_hash()
 
-
-
-
 # This creates a server connection to the same URL that contains the graphic interface for Blazegraph.
 sparql = SPARQLWrapper("http://localhost:9999/blazegraph/sparql")
 
 
 def item_lifter(i1, i2):
-
     item_list = [i1, i2]
     for i in item_list:
 
         sparql.setQuery("""
                 PREFIX nhterm: <https://newshunter.uib.no/term#>
-                SELECT ?time ?text ?irl ?anchor ?contributor ?annotator ?entity ?collection
+                SELECT ?time ?text ?irl ?anchor ?contributor ?annotator ?collection
                     WHERE
                     {
                     ?item a nhterm:Item ;
@@ -45,15 +42,14 @@ def item_lifter(i1, i2):
                         nhterm:sourceIRL ?irl ;
                         nhterm:hasContributor ?contributor ;
                         nhterm:inCollection ?collection ;
-                        
+
                     nhterm:hasAnnotation ?superAnnotation1 .
                     ?superAnnotation1 nhterm:anchorOf ?anchor .
                     ?superAnnotation1 nhterm:hasAnnotator ?annotator .
-                    ?superAnnotation1 nhterm:hasEntity ?entity .
                     FILTER(STR(?item) ="%s")
                     }
-                    LIMIT 10
-            """%i)
+                    LIMIT 5
+            """ %i)
         sparql.setReturnFormat(JSON)
 
         if i == i1:
@@ -69,32 +65,37 @@ def item_lifter(i1, i2):
 
 sparql.setQuery("""
 PREFIX nhterm: <https://newshunter.uib.no/term#>
-    SELECT DISTINCT ?item1 ?item2 ?value  WHERE {
+    SELECT DISTINCT ?item1 ?item2 ?entity1 ?entity2 WHERE {
 
         ?item1 a nhterm:Item ;
         nhterm:hasAnnotation ?superAnnotation1 .
-        ?superAnnotation1 nhterm:hasEntity ?value . 
-    
+        ?superAnnotation1 nhterm:hasEntity ?entity1 . 
+
         ?item2 a nhterm:Item ;
         nhterm:hasAnnotation ?superAnnotation2 .
-        ?superAnnotation2 nhterm:hasEntity ?value . 
+        ?superAnnotation2 nhterm:hasEntity ?entity2 . 
         FILTER(?item1 != ?item2)
+        FILTER(?entity1 = ?entity2)
     }
-    LIMIT 10
+
+    LIMIT 5
 """)
 sparql.setReturnFormat(JSON)
-entity = sparql.query().convert()
+entity_graph = sparql.query().convert()
 
 g = Graph()
 
-for items in entity["results"]["bindings"]:
+for items in entity_graph["results"]["bindings"]:
     item1 = items["item1"]["value"]
     item2 = items["item2"]["value"]
+    #print("entity1", items["entity1"]["value"])
+    #print("entity2", items["entity2"]["value"])
+
     item_output = item_lifter(item1, item2)
 
     output_dict = {"time": item_output["time"]["value"], "irl": item_output["irl"]["value"],
                    "anchor": item_output["anchor"]["value"], "annotator": item_output["annotator"]["value"],
-                   "entity": item_output["entity"]["value"], "collection": item_output["collection"]["value"]}
+                   "collection": item_output["collection"]["value"]}
 
     # ------------------- Make Event graph -------------------
 
@@ -141,69 +142,13 @@ for items in entity["results"]["bindings"]:
     g.add((bn2, nhterm.hasDescriber, URIRef(output_dict["collection"])))
 
     # nhterm:hasEntity
-    g.add((bn2, nhterm.hasEntity, URIRef(output_dict["entity"])))
+    #g.add((bn2, nhterm.hasEntity, URIRef(entity)))
 
     # nhterm:sourceIRL
     g.add((bn2, nhterm.sourceIRL, URIRef(output_dict["irl"])))
 
 print(g.serialize(format="ttl").decode("utf-8"))
 
-
 # java -server -Xmx4g -jar blazegraph.jar
 
 
-# -------------Items------------#
-
-"""
-AnchorOf
-
-Return two items where the items match on having a Annotation 
-where the Annotation is nhterm:anchorOf 
-and the value is the same in both items
-"""
-
-"""Return items ordered by date and organized by items grouped from date and time"""
-
-d = 18
-
-sparql.setQuery("""
-    PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-    PREFIX nhterm: <https://newshunter.uib.no/term#>
-        SELECT ?item ?dt ?year ?month ?day ?hours ?minutes 
-            WHERE
-            {
-                ?item a nhterm:Item ;
-                nhterm:sourceDateTime ?dt .
-                bind(year(?dt) as ?year)
-                bind(month(?dt) as ?month)
-                bind(day(?dt) as ?day)
-                bind(hours(?dt) as ?hours)
-                bind(minutes(?dt) as ?minutes)
-                FILTER((?year = 2020 && ?month = 9 && ?day = %s)) 
-            }
-        ORDER BY ?year ?month ?day ?hours ?minutes
-        
-"""%d)
-
-""" Constructs a graph object from the return value of the above query"""
-time_results = sparql.query().convert()
-#for content in time_results["results"]["bindings"]:
-    #print(content)
-
-
-'''
-for result in time_results["results"]["bindings"]:
-    item = result["item"]["value"]
-
-    sparql.setQuery("""
-           DESCRIBE ?item WHERE
-           {
-              FILTER(STR(?item) = "%s")
-           }
-      """ % item)
-    sparql.setReturnFormat(XML)
-
-output_graph = sparql.queryAndConvert()
-serialized_graph = output_graph.serialize(format="ttl").decode("utf-8")
-print(serialized_graph)
-'''
